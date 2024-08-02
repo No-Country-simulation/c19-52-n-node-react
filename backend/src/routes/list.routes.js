3d68380acea850bcfb08fc59556f82714ae3ee34
Henryck Guaramato <guaramatohenryck@gmail.com>
2024-08-01 23:36:28 -0400

    Arreglo de middleware

--- backend/src/routes/list.routes.js
+++ backend/src/routes/list.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import ListController from "../controllers/list.controller.js";
-import { authToken, checkOwner } from "../middlewares/auth.js";
+import { authToken, checkVisibility } from "../middlewares/auth.js";
 
 const controller = new ListController()
 
 const listRouter = Router()
 
 listRouter.get('/', authToken, controller.getListsByUser)
-listRouter.get('/:lid', checkOwner, controller.getListById)
+listRouter.get('/:lid', checkVisibility, controller.getListById)
 listRouter.post('/', authToken, controller.createList)
 listRouter.post('/private/:lid', authToken, controller.changeToPrivate)
 listRouter.post('/public/:lid', authToken, controller.changeToPublic)
